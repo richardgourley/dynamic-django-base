f8af3ebc37d883e182653ecb7612a2202632aa81
Richard G <codeleap@outlook.com>
2022-07-17 10:51:30 +0200

    Delete urls.py

--- dynamic-django-base/flatpages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import FlatPageDetailView
-
-app_name = 'flatpages'
-urlpatterns = [
-    path('<slug:slug>', FlatPageDetailView.as_view(), name="flatpage_detail"),
-]
