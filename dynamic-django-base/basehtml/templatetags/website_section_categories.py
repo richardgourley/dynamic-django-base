d29c19932523bdd9a1a4f2443c41cea67eb2a818
Richard G <codeleap@outlook.com>
2022-07-17 10:54:49 +0200

    Delete website_section_categories.py

--- dynamic-django-base/basehtml/templatetags/website_section_categories.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from websitesections.models import WebsiteSectionCategory
-
-register = template.Library()
-
-@register.simple_tag
-def get_website_section_categories():
-	website_section_categories = WebsiteSectionCategory.objects.all().order_by('section_category_title')
-	return website_section_categories
