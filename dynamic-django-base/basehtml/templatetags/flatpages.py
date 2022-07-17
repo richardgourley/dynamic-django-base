e87f2375a505ccd0ac9dfe6c11e21f8533b3d001
Richard G <codeleap@outlook.com>
2022-07-17 10:54:37 +0200

    Delete flatpages.py

--- dynamic-django-base/basehtml/templatetags/flatpages.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from flatpages.models import FlatPage
-
-register = template.Library()
-
-@register.simple_tag
-def get_flatpages():
-	flatpages = FlatPage.objects.all().order_by('page_order')
-	return flatpages
